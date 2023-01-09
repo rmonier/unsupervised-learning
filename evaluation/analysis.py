a0dd361ee5cc4ae83b858c33508c21061a09420a
rmonier <romain.monier0609@gmail.com>
2023-01-09 20:22:13 +0100

    WIP SOM

--- evaluation/analysis.py
+++ evaluation/analysis.py
@@ -1,1 +1,39 @@
-#TODO: SOM analysis
+import subprocess
+
+def main():
+    activations = ["sigmoid"]
+    learning_rates = [0.01, 0.05, 0.1, 0.15, 0.2]
+    momentums = [0, 0.1, 0.5, 0.7, 0.9]
+    epochs = [100, 500, 1000]
+    topolgy = [[9,5], [4,6], [9], [4,6,8], [8,3]]
+    for activation in activations:
+        for learning_rate in learning_rates:
+            for momentum in momentums:
+                for epoch in epochs:
+                    for hidden_layer in topolgy:
+                        out_filename = f"som_A3-data_{activation}_{epoch}_{learning_rate}_{momentum}_4-"
+                        hl = "[4; "
+                        for i in hidden_layer:
+                            hl += f"{i}; "
+                            out_filename += f"{i}-"
+                        hl += "1]"
+                        out_filename += "1.csv"
+                        cmd = f"python network/som.py \"datasets/preprocessed/A3-data\" \"{hl}\" {activation} {learning_rate} {momentum} {epoch} \"{out_filename}\""
+                        print(f"{cmd}\n")
+                        subprocess.run(cmd)
+                        print("\n")
+                        
+                        out_filename = f"som_A3-top10s_{activation}_{epoch}_{learning_rate}_{momentum}_9-"
+                        hl = "[9; "
+                        for i in hidden_layer:
+                            hl += f"{i}; "
+                            out_filename += f"{i}-"
+                        hl += "1]"
+                        out_filename += "1.csv"
+                        cmd = f"python network/som.py \"datasets/split/A3-top10s\" \"{hl}\" {activation} {learning_rate} {momentum} {epoch} \"{out_filename}\""
+                        print(f"{cmd}\n")
+                        subprocess.run(cmd)
+                        print("\n")
+
+if __name__ == '__main__':
+    main()
--- network/som.py
+++ network/som.py
@@ -1,7 +1,35 @@
-from sklearn_som import SOM
+from sklearn_som.som import SOM
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import pandas as pd
 
-model = SOM(5, 5, 2, sigma=1.0, learning_rate=0.5)
-X = [[5,3], [10,15], [15,12], [24,10], [30,45], [85,70], [71,80], [60,78], [55,52], [80,91]]
-model.fit(X)
+# Load dataset
+dataset = pd.read_csv('datasets/preprocessed/A3-data.csv', encoding='utf8', sep=';')
+# data is everythin except the label column named class
+data = dataset.drop("class", axis=1).values
+# select label column named class
+label = dataset["class"].values
+print(data)
+print(label)
+# Extract just two features (just for ease of visualization)
+data = data[:, :2]
 
-model.visualize()
+som = SOM(5, 5, 2, sigma=1.0, lr=0.5)
+
+# Fit it to the data
+som.fit(data)
+
+predictions = som.predict(data)
+
+# Plot the results
+fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(5,7))
+x = data[:,0]
+y = data[:,1]
+colors = ['red', 'green', 'blue']
+
+ax[0].scatter(x, y, c=label, cmap=ListedColormap(colors))
+ax[0].title.set_text('Actual Classes')
+ax[1].scatter(x, y, c=predictions, cmap=ListedColormap(colors))
+ax[1].title.set_text('SOM Predictions')
+
+plt.show()
